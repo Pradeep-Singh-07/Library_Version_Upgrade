3cf4d3c7e34a6167d395d65e93bee0ba7063a788
Pradeep Singh <pradeepanddhruv@gmail.com>
2023-06-12 21:57:44 -0700

    applied binary search to find root version upgrade

--- dependency_extraction.js
+++ dependency_extraction.js
@@ -1,50 +1,134 @@
-const { exec } = require('child_process');
-const fs=require('fs');
-allpackages =new Set();
-
-function stformat(packageName,packageVersion){
-    return packageName+'$#@$#@'+packageVersion;
-}
-
-function inv_stformat(formatedpackagestring){
-    return formatedpackagestring.split('$#@$#@');
-}
-
-
+let {exec}=require('child_process');
 function cleanup(out){
-    
     out=out.slice(1,-2);
-    out=out.split(',').join('\n').split("'").join('').split('\n');
-    out=out.join('$').split('^').join('').split('$');
-    
-    out=out.map((item)=>item.split(':'));
-    out.forEach(element => {
-        if(element[1]){
-        packageDependencies(element[0],element[1]);
-    }    
-    });
+    let arr =out.split("'").join("")
+                .split('\n').join('')
+                .split(' ').join('')
+                .split('^').join('')
+                .split('~').join('')
+                .split('<').join('')
+                .split('=').join('')
+                .split('>').join('')
+                .split('|').join('')
+                .split(',');
+    arr=arr.map((item)=>item.split(':'));
+    if(arr.length==1 && arr[0]=='')arr=[];
+    return arr;
 }
-
-
-
-function packageDependencies(packageName,packageVersion){
-    formatedpackagestring=stformat(packageName,packageVersion);
-    if(!allpackages.has(formatedpackagestring)){
-        allpackages.add(formatedpackagestring);
+let dependencyCache=new Map();
+let versionCache=new Map();
+function stringify(a){
+    return a.join('@#$%');
+}
+function destringify(a){
+    return a.split('@#$%');
+}
+ function directDependency(packageName,packageVersion){
+    return new Promise((resolve,reject)=>{
         exec(`npm view ${packageName}@${packageVersion} dependencies`,(err,out)=>{
+            if(err)throw err;
+            let result = cleanup(out);
 
-             if(err)throw err;
-             cleanup(out);
-            });
-        }
+            resolve(result);
+        });
+    })
+}
+async function getDirectDependencies(packageName,packageVersion){
+    
+    return new Promise(async (resolve,reject)=>{
+    let package=[packageName,packageVersion];
+    if(dependencyCache.has(stringify(package))){
+
+        resolve(  dependencyCache.get(stringify(package)));
+    }
+    let result = await directDependency(packageName,packageVersion);
+    dependencyCache.set(stringify(package),result);
+    resolve(result);
+    })
+   
+    
+}
+async function getAllDependencies(packageName,packageVersion){
+    return new Promise(async (resolve,reject)=>{
+    let alldependencies=new Set();
+    let stack=[[packageName,packageVersion]];
+    while(stack.length){
+        stack.forEach((item)=>{alldependencies.add(stringify(item));})
+        let stack2=[];
+        stack=stack.map((item)=>getDirectDependencies(item[0],item[1]));
+         stack = await Promise.all(stack);
+        stack.forEach((item)=>stack2.push(...item));
+        stack2.filter((item)=>!alldependencies.has(stringify(item)));
+        stack=stack2;
+    }
+    resolve([...alldependencies].map((item)=>destringify(item)));})
 }
 
- packageDependencies('react-use','12.13.0');
- setTimeout(() => {
-    for(let value of allpackages){
-        console.log(inv_stformat(value));
+async function extractVersions(packageName){
+    return new Promise((resolve,reject)=>{
+        exec(`npm view ${packageName} versions`,(err,out)=>{
+            out=out.slice(1,-2).split(' ').join('').split('\n').join('').split("'").join('').split(',');
+            if(err)throw err;
+            resolve(out);
+        })
+    })
+}
+async function getver(packageName){
+    return new Promise(async (resolve,reject)=>{
+    
+    if(versionCache.has(`${packageName}`)){
+        resolve (versionCache.get(`${packageName}`));
     }
-},10000);
+    let out= await extractVersions(packageName);  
+    versionCache.set(`${packageName}`,out);
+    resolve(out);  
+})
+}
+let rootPackageName='react-use';
+let rootPackageVersion='10.0.0';
+
+async function mainfun(dependencyName,dependencyDestinationVersion){
+    let rootPackageVersions = await getver(rootPackageName);
+    let dependencyVersions = await getver(dependencyName);
+    
+    
+    let inverseRootPackageVersions =new Map();
+    let inverseDependencyVersions=new Map();
+    rootPackageVersions.forEach((item,idx)=>inverseRootPackageVersions.set(item,idx));
+    dependencyVersions.forEach((item,idx)=>inverseDependencyVersions.set(item,idx));
+    let rootVersionCount = rootPackageVersions.length;
+    let rootindex=rootVersionCount-1,bit=1<<10,last=-1;
+   
+  
+    while(1){
+        if(rootindex-bit>0){
+            let allcurrentdependencies=await getAllDependencies(rootPackageName,rootPackageVersions[rootindex-bit]);
+            let thisdependency=allcurrentdependencies.filter((item)=>item[0]==`${dependencyName}`);
+            if(thisdependency.length && Number(inverseDependencyVersions.get(`${thisdependency[0][1]}`))>=Number(inverseDependencyVersions.get(`${dependencyDestinationVersion}`))){
+                rootindex-=bit;
+                last=thisdependency[0][1];
+            }
+        }
+        bit/=2;
+        if(bit<1){
+            if(last==dependencyDestinationVersion){
+                console.log(`upgrade to ${rootPackageVersions[rootindex]}`);
+            }
+            else {
+                console.log('no favourable outcome')
+            }
+             break;
+            
+            
+        }
+        
+    }
+
+}
+mainfun('throttle-debounce','3.0.1');
+
+   
+
 
 
 
